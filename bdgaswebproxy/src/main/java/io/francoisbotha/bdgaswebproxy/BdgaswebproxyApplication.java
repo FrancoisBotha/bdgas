82c490a50b78aca2fa1c7335754bac66a160765f
Francois Botha <botha.francois@gmail.com>
2018-06-24 14:53:03 +0200

    Web Proxy Inititial Commit

---
+++ bdgaswebproxy/src/main/java/io/francoisbotha/bdgaswebproxy/BdgaswebproxyApplication.java
@@ -0,0 +1,12 @@
+package io.francoisbotha.bdgaswebproxy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BdgaswebproxyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BdgaswebproxyApplication.class, args);
+    }
+}
