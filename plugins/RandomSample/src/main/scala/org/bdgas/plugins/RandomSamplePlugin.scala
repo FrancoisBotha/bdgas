b59beb3a077b42484637ed8abc611aba764fa79a
FrancoisBotha <francois@ims.com.na>
2018-09-20 22:59:26 +0200

    docker read file from local file system

--- plugins/RandomSample/src/main/scala/org/bdgas/plugins/RandomSamplePlugin.scala
+++ plugins/RandomSample/src/main/scala/org/bdgas/plugins/RandomSamplePlugin.scala
@@ -1,5 +1,117 @@
 package org.bdgas.plugins
 
-object RandomSamplePlugin {
+import com.typesafe.config.Config
+import org.apache.commons.lang.StringUtils
+import org.apache.spark.sql.{DataFrame, Dataset, Row, SparkSession}
+import org.scalactic._
+import spark.jobserver.SparkSessionJob
+import spark.jobserver.api.{JobEnvironment, SingleProblem, ValidationProblem}
 
+import scala.util.Try
+
+object RandomSamplePlugin extends SparkSessionJob {
+  type JobData = Seq[String]
+  type JobOutput = String
+
+  def runJob(sparkSession: SparkSession, runtime: JobEnvironment, data: JobData): JobOutput = {
+
+      val df = sparkSession
+          .read
+          .option(" inferSchema", "true")
+          .option(" header", "true")
+          .csv("file:///mnt/data/Invoices10K.txt")
+
+       val a = df.selectExpr("min('Amt'), max('Amt')")
+       showString(a)
+
+//      val seed = 5
+//      val withReplacement = false
+//      val fraction = 0.1
+//      df.sample( withReplacement, fraction, seed)
+
+  }
+
+  def validate(sparkSession: SparkSession, runtime: JobEnvironment, config: Config):
+  JobData Or Every[ValidationProblem] = {
+    Try(config.getString("input.string").split(" ").toSeq)
+      .map(words => Good(words))
+      .getOrElse(Bad(One(SingleProblem("No input.string param"))))
+  }
+
+  def showString(df:DataFrame,_numRows: Int = 20, truncate: Int = 20): String = {
+    val numRows = _numRows.max(0)
+    val takeResult = df.take(numRows + 1)
+    val hasMoreData = takeResult.length > numRows
+    val data = takeResult.take(numRows)
+
+    // For array values, replace Seq and Array with square brackets
+    // For cells that are beyond `truncate` characters, replace it with the
+    // first `truncate-3` and "..."
+    val rows: Seq[Seq[String]] = df.schema.fieldNames.toSeq +: data.map { row =>
+      row.toSeq.map { cell =>
+        val str = cell match {
+          case null => "null"
+          case binary: Array[Byte] => binary.map("%02X".format(_)).mkString("[", " ", "]")
+          case array: Array[_] => array.mkString("[", ", ", "]")
+          case seq: Seq[_] => seq.mkString("[", ", ", "]")
+          case _ => cell.toString
+        }
+        if (truncate > 0 && str.length > truncate) {
+          // do not show ellipses for strings shorter than 4 characters.
+          if (truncate < 4) str.substring(0, truncate)
+          else str.substring(0, truncate - 3) + "..."
+        } else {
+          str
+        }
+      }: Seq[String]
+    }
+
+    val sb = new StringBuilder
+    val numCols = df.schema.fieldNames.length
+
+    // Initialise the width of each column to a minimum value of '3'
+    val colWidths = Array.fill(numCols)(3)
+
+    // Compute the width of each column
+    for (row <- rows) {
+      for ((cell, i) <- row.zipWithIndex) {
+        colWidths(i) = math.max(colWidths(i), cell.length)
+      }
+    }
+
+    // Create SeparateLine
+    val sep: String = colWidths.map("-" * _).addString(sb, "+", "+", "+\n").toString()
+
+    // column names
+    rows.head.zipWithIndex.map { case (cell, i) =>
+      if (truncate > 0) {
+        StringUtils.leftPad(cell, colWidths(i))
+      } else {
+        StringUtils.rightPad(cell, colWidths(i))
+      }
+    }.addString(sb, "|", "|", "|\n")
+
+    sb.append(sep)
+
+    // data
+    rows.tail.map {
+      _.zipWithIndex.map { case (cell, i) =>
+        if (truncate > 0) {
+          StringUtils.leftPad(cell.toString, colWidths(i))
+        } else {
+          StringUtils.rightPad(cell.toString, colWidths(i))
+        }
+      }.addString(sb, "|", "|", "|\n")
+    }
+
+    sb.append(sep)
+
+    // For Data that has more than "numRows" records
+    if (hasMoreData) {
+      val rowsString = if (numRows == 1) "row" else "rows"
+      sb.append(s"only showing top $numRows $rowsString\n")
+    }
+
+    sb.toString()
+  }
 }
